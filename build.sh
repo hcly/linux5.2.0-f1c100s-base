69f4d7cff106f3e0cd881b581decd3ad7cf389b6
leijie <leijie@icoolarm.com>
2019-12-26 13:49:15 +0800

    修改build.sh增加rtp配置

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 CPU_NUM=$(cat /proc/cpuinfo |grep processor|wc -l)
 CPU_NUM=$((CPU_NUM+1))
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- sunxi_f1c100s_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j${CPU_NUM}
+make ARCH=arm CROSS_COMPILE=arm-f1c100s-linux-gnueabi- sunxi_f1c100s_defconfig
+make ARCH=arm CROSS_COMPILE=arm-f1c100s-linux-gnueabi- -j${CPU_NUM}
 
 cp arch/arm/boot/zImage /home/lei/lshare/
 cp arch/arm/boot/dts/suniv-f1c100s-licheepi-nano.dtb /home/lei/lshare/
